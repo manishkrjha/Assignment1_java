a834273f738f6830a865786285b1692d5ec01956
Manish Jha <codernibba@gmail.com>
2023-01-31 00:41:53 +0530

    add comments

--- Calculator/src/methods/Calculator.java
+++ Calculator/src/methods/Calculator.java
@@ -1,25 +1,36 @@
 package methods;
 
+// Importing required modules
 import java.util.ArrayList;
 import java.util.List;
 public class Calculator {
+
+//    Creating arraylist to store all the values will be added
     List<Double> arr = new ArrayList<Double>();
 
+//    Defining method to add the number to the arrayList.
+//    This takes a double value as input and adds it to the arraylist
     public void addNumber(double val){
         arr.add(val);
     }
 
+//    Method to calculate the average of the numbers
     public void average(){
+
+//        Getting total number of elements
         int numberOfElements = arr.size();
 
         double sum = 0;
 
+//        Calculating sum of all numbers
         for(double e: arr){
             sum += e;
         }
 
+//        Logic to calculate the average
         double avg = sum/numberOfElements;
 
+//        Displaying output by rounding it off to two digits (as instructed in the assignment)
         System.out.println(Math.round(avg*100.0)/100.0);
     }
 }
--- Calculator/src/methods/Tester.java
+++ Calculator/src/methods/Tester.java
@@ -1,21 +1,30 @@
 package methods;
 import java.util.Scanner;
+
+//Defining Tester class to test the program
 public class Tester {
     public static void main(String[] args){
+
+//        Creating scanner object to take the input
         Scanner sc = new Scanner(System.in);
 
+//        Taking the value of total number of elements to be inserted
         System.out.println("Enter the number of elements: ");
         int numberOfElements = sc.nextInt();
 
         Calculator c = new Calculator();
 
         for(int i=1;i<=numberOfElements;i++){
+
+//            Inputting each number
             System.out.println("Enter the number: ");
             double num = sc.nextDouble();
 
+//            Inserting the elements
             c.addNumber(num);
         }
 
+//        Calculating and displaying the average
         c.average();
     }
 
